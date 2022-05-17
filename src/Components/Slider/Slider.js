a80a9ce207aad2251eea4af43ff3ec8d2c54107b
Thiago Lins <taglins@gmail.com>
2022-05-16 21:04:39 -0300

    update utilities

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import { Autoplay, Navigation } from "swiper";
-
+import * as S from "./styles";
 const Slider = ({ technologies }) => {
-    return (
-        <Swiper
-            tag='section'
-            wrapperTag='ul'
-            spaceBetween={30}
-            slidesPerView={3}
-            centeredSlides={true}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            navigation={true}
-            modules={[Autoplay, Navigation]}
-        >
-            {technologies.map((tech, i) => {
-                return (
-                    <SwiperSlide tag='li' key={`slide-${i}`}>
-                        <img src={tech} alt={`slide-${i}`} />
-                    </SwiperSlide>
-                )
-            })}
-        </Swiper>
-    )
-}
+  return (
+    <S.Wrapper>
+      <Swiper
+        tag="div"
+        wrapperTag="ul"
+        spaceBetween={5}
+        slidesPerView={4}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+      >
+        {technologies.map((tech, i) => {
+          return (
+            <SwiperSlide tag="li" key={`slide-${i}`}>
+              <img src={`./assets/img/${tech}.png`} alt={`slide-${i}`} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </S.Wrapper>
+  );
+};
 
 export default Slider;
--- src/Components/Utilities/Utilities.js
+++ src/Components/Utilities/Utilities.js
@@ -1,78 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
-
-import Titulo from '../Titulo/Titulo';
-import Slider from '../Slider/Slider';
-
-import bootstrap from '../Slider/img/bootstrap.png';
-import angular from '../Slider/img/angular.png';
-import go from '../Slider/img/go.png';
-import html from '../Slider/img/html5.png';
-import css from '../Slider/img/css3.png';
-import docker from '../Slider/img/docker.png';
-import java from '../Slider/img/java.png';
-import javascript from '../Slider/img/javascript.png';
-import nodejs from '../Slider/img/nodejs.png';
-import python from '../Slider/img/python.png';
-import raspberry from '../Slider/img/raspberry.png';
-import react from '../Slider/img/react.png';
-import sass from '../Slider/img/sass.png';
-import swift from '../Slider/img/swift.png';
-import vuejs from '../Slider/img/vuejs.png';
-import Arrow from './img/arrow.png';
-
-const Box = styled.div`
-    margin: 0 6rem;
-`
-
-const BoxIntern = styled.div`
-    margin-left: 4rem;
-`
-
-const Title = styled.h1`
-    font-size: 4.5rem;
-    font-family: 'Abril Fatface', cursive;
-    transition: all .2s;
-`
-
-const Subtitle = styled.h2`
-    font-size: 8rem;
-    margin-top: -1rem;
-    margin-bottom: 1rem;
-    font-family: 'Ms Madi', cursive;
-`
-
-const BoxTitle = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 50px;
-`
-
-const Icon = styled.img`
-    width: 60px;
-`
-
-const learning = [javascript, nodejs, react, html, css, sass, bootstrap];
-const learn = [angular, vuejs, java, python, go, raspberry, swift, docker];
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import * as S from "./styles";
+import Titulo from "../Titulo/Titulo";
+import Slider from "../Slider/Slider";
+const learning = [
+  "javascript",
+  "nodejs",
+  "react",
+  "html5",
+  "css3",
+  "sass",
+  "bootstrap",
+];
+const learn = [
+  "angular",
+  "vuejs",
+  "java",
+  "python",
+  "go",
+  "raspberry",
+  "swift",
+  "docker",
+];
 const Utilities = () => {
-    return (
-        <Box>
-            <BoxTitle>
-                <NavLink to='/'>
-                    <Icon src={Arrow} alt="Voltar à página principal" />
-                </NavLink>
-                <Title>Utilities</Title>
-            </BoxTitle>
-            <BoxIntern>
-                <Titulo frase='Qual a minha stack foco?' />
-                <Subtitle>Front-end</Subtitle>
-                <Titulo frase='Quais tecnologias eu estudo?' />
-                <Slider technologies={learning} />
-            </BoxIntern>
-        </Box>
-    )
-}
+  return (
+    <S.Box>
+      <S.BoxTitle>
+        <NavLink to="/">
+          <S.Icon
+            src={`./assets/img/arrow.png`}
+            alt="Voltar à página principal"
+          />
+        </NavLink>
+        <S.Title>Utilities</S.Title>
+      </S.BoxTitle>
+      <S.BoxIntern>
+        <Titulo frase="Qual a minha stack foco?" />
+        <S.Subtitle>Front-end</S.Subtitle>
+        <Titulo frase="Quais tecnologias eu estudo?" />
+        <Slider technologies={learning} />
+      </S.BoxIntern>
+    </S.Box>
+  );
+};
 
 export default Utilities;
