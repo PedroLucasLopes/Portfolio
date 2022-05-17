7927af4730bb783d8e13083007b59111078ffafa
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2022-05-16 21:24:21 -0300

    correct conflict

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -2,35 +2,6 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay, Navigation } from "swiper";
-<<<<<<< HEAD
-import 'swiper/css';
-
-const Slider = ({ technologies }) => {
-    return (
-        <Swiper
-            tag='section'
-            wrapperTag='ul'
-            spaceBetween={30}
-            slidesPerView={8}
-            centeredSlides={true}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            navigation={true}
-            modules={[Autoplay, Navigation]}
-        >
-            {technologies.map((tech, i) => {
-                return (
-                    <SwiperSlide tag='li' key={`slide-${i}`}>
-                        <img src={tech} alt={`slide-${i}`} />
-                    </SwiperSlide>
-                )
-            })}
-        </Swiper>
-    )
-}
-=======
 import * as S from "./styles";
 const Slider = ({ technologies }) => {
   return (
@@ -59,6 +30,5 @@ const Slider = ({ technologies }) => {
     </S.Wrapper>
   );
 };
->>>>>>> a80a9ce207aad2251eea4af43ff3ec8d2c54107b
 
 export default Slider;
